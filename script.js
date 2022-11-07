cccc12a494552cae5dcca5c334ae7df4341c03a1
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2022-11-07 15:08:09 -0500

    added javascript skeleton for pop up prompts

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var charLowerCase = `abcdefghijklmnopqrstuvwxyz`;
+var charUpperCase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+var charNumber = `0123456789`;
+var charSpecial = `!"#$%&'()*+,-./:;<=>?@[\]^_{|}~`;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-function generatePassword(){
-  
+
+function generatePassword() {
+  var charList = "";
+
+  var passwordLength = prompt(
+    `Enter password length (must be between 8 and 128 characters)`
+  );
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("invalid!");
+    return;
+  }
+
+  var isLowerCase = confirm(
+    "Click OK if password requires lowercase characters"
+  );
+
+  if (isLowerCase) {
+    charList += charLowerCase;
+  }
+
+  var isUpperCase = confirm(
+    "Click OK if password requires uppercase characters"
+  );
+
+  if (isUpperCase) {
+    charList += charUpperCase;
+  }
+
+  var isNumber = confirm("Click OK if password requires a number");
+
+  if (isNumber) {
+    charList += charNumber;
+  }
+
+  var isSpecial = confirm("Click OK if password requires a special character");
+
+  if (isSpecial) {
+    charList += charSpecial;
+  }
+
+  return charList;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
